a1fa20e8783c4d29c204aef63c7d9018b0d771be
Quantum Fusion <10088099+quantum-fusion@users.noreply.github.com>
2018-08-26 17:33:01 -0400

    Update SpringBootWebApplication.java

--- src/main/java/com/server/springboot/SpringBootWebApplication.java
+++ src/main/java/com/server/springboot/SpringBootWebApplication.java
@@ -8,7 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.Arrays;
 
-
+//
 @SpringBootApplication
 public class SpringBootWebApplication {
     public static void main(String[] args) {
